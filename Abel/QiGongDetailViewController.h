be206482b253aaa68bc9d6a7cff4389a370da5ad
Jorge Laguna <jorge.laguna@omnilife.com>
2014-05-23 09:28:22 -0500

    Initial Commit

---
+++ Abel/QiGongDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  QiGongDetailViewController.h
+//  Abel
+//
+//  Created by Webmaster on 23/05/14.
+//  Copyright (c) 2014 Abel Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiGongDetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
