0e3670379bf973b4aad418088c988ccc853c1b61
Jorge Laguna <jorge.laguna@omnilife.com>
2014-06-20 13:32:19 -0500

    Initial Commit

---
+++ animacion20jun/animacion20jun/animacion20jun/XYZViewController.m
@@ -0,0 +1,29 @@
+//
+//  XYZViewController.m
+//  animacion20jun
+//
+//  Created by Webmaster on 20/06/14.
+//  Copyright (c) 2014 Omnilife. All rights reserved.
+//
+
+#import "XYZViewController.h"
+
+@interface XYZViewController ()
+
+@end
+
+@implementation XYZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ animacion20junz/Animation/Animation/XYZViewController.h
@@ -0,0 +1,22 @@
+//
+//  XYZViewController.h
+//  Animation
+//
+//  Created by Webmaster on 20/06/14.
+//  Copyright (c) 2014 Omnilife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController {
+
+IBOutlet UIImageView * image;
+
+
+}
+
+-(IBAction)play;
+-(IBAction)stop;
+
+
+@end
